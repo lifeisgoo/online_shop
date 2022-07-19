91fdc576f4f28a87f0e568941c60f51a33e3d37b
lifeisgoo <abdulatibovao@gmail.com>
2022-07-19 21:51:50 +0500

    ;;;;

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+class ContactModel(models.Model):
+    name = models.CharField(max_length=32, verbose_name=_('name'))
+    email = models.EmailField(verbose_name=_('email'))
+    massage = models.TextField(verbose_name=_('massage'))
+    created_at = models.DateTimeField( auto_now_add=True ,verbose_name=_('created_at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('contact')
+        verbose_name_plural = _('contacts')
