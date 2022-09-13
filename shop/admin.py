63a478adf2b4f4b270a6afb9d1c429345c4df343
lifeisgoo <abdulatibovao@gmail.com>
2022-09-13 22:07:47 +0500

    added size color brand models

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,33 @@
 from django.contrib import admin
-from .models import ProductTagModel, ProductModel, CategoryModel
+from .models import ProductTagModel, ProductModel, CategoryModel, BrandModel, ColorModel, SizeModel
+
 
 @admin.register(ProductTagModel)
 class ProductTagModelAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_display_links = ['name']
     search_fields = ['name']
+
+
+@admin.register(SizeModel)
+class SizeModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+@admin.register(BrandModel)
+class BrandModelAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_display_links = ['name']
+    search_fields = ['name']
+
+
+@admin.register(ColorModel)
+class ColorModelAdmin(admin.ModelAdmin):
+    list_display = ['code']
+    list_display_links = ['code']
+    search_fields = ['code']
 
 
 @admin.register(CategoryModel)
@@ -21,4 +43,18 @@ class ProductModelAdmin(admin.ModelAdmin):
     list_display_links = ['title', 'price', 'created_at']
     list_filter = ['created_at']
     search_fields = ['title', 'price']
-    autocomplete_fields = ['category', 'tag']
+    autocomplete_fields = ['category', 'tag', 'size', 'colors']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- shop/models.py
+++ shop/models.py
@@ -30,6 +30,45 @@ class ProductTagModel(models.Model):
         verbose_name_plural = 'tags'
 
 
+class BrandModel(models.Model):
+    name = models.CharField(max_length=60, verbose_name=_('name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'brand'
+        verbose_name_plural = 'brands'
+
+
+class SizeModel(models.Model):
+    name = models.CharField(max_length=60, verbose_name=_('name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'size'
+        verbose_name_plural = 'sizes'
+
+
+class ColorModel(models.Model):
+    code = models.CharField(max_length=60, verbose_name=_('name'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = 'color'
+        verbose_name_plural = 'colors'
+
+
+
+
+
 class ProductModel(models.Model):
     category = models.ForeignKey(
         CategoryModel, on_delete=models.RESTRICT,
@@ -42,11 +81,10 @@ class ProductModel(models.Model):
     price = models.FloatField(verbose_name=_('price'))
     sale = models.PositiveSmallIntegerField(default=0, verbose_name=_('sale'))
     main_image = models.ImageField(upload_to='products/', verbose_name=_('main_image'))
-    tag = models.ManyToManyField(
-        ProductTagModel,
-        related_name='products',
-        verbose_name=_('tags')
-    )
+    tag = models.ManyToManyField(ProductTagModel,related_name='products',verbose_name=_('tags'))
+    size = models.ManyToManyField(SizeModel, related_name='products', verbose_name=_('sizes'))
+    brand = models.ForeignKey(BrandModel, on_delete=models.RESTRICT,related_name='products', verbose_name=_('brands'), null=True)
+    colors = models.ManyToManyField(ColorModel, related_name='products', verbose_name=_('colors'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
 
     def get_price(self):
